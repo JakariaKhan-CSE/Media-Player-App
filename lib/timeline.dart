13b3606ddd0070091fca61deb603245669e4b87e
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2025-02-06 20:10:38 +0600

    task complete, show listwheelscrollview builder with dumy data(10 list) when click add button it insert new text in position 3.

--- lib/timeline.dart
+++ lib/timeline.dart
@@ -12,23 +12,49 @@ class _TimeLineScreenState extends State<TimeLineScreen> {
     10,
     (index) => index + 1,
   );
+
+  void addItemAtPosition(int index) {
+    setState(() {
+      items.insert(index, -1); // -1 represents the new element "NEW"
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     return SizedBox(
         height: height,
-        child: ListWheelScrollView.useDelegate(
-            itemExtent: 200,
-            childDelegate: ListWheelChildBuilderDelegate(
-              childCount: items.length,
-              builder: (context, index) {
-                return Container(
-                  height: 200,
-                  width: double.infinity,
-                  color: Colors.pink,
-                  child: Center(child: Text('${index}')),
-                );
-              },
-            )));
+        child: Column(
+          children: [
+            Expanded(
+              child: ListWheelScrollView.useDelegate(
+                  itemExtent: 200,
+                  childDelegate: ListWheelChildBuilderDelegate(
+                    childCount: items.length,
+                    builder: (context, index) {
+                      return Container(
+                        height: 200,
+                        width: double.infinity,
+                        color: Colors.pink,
+                        child: Center(
+                            child: Text(
+                          items[index] == -1 ? 'NEW' : items[index].toString(),
+                          style: TextStyle(
+                              fontSize: 28, fontWeight: FontWeight.w800),
+                        )),
+                      );
+                    },
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    addItemAtPosition(3); // Insert at position 3
+                  },
+                  child: Text('ADD')),
+            )
+          ],
+        ));
   }
 }
