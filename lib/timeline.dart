fe12a7ad6abae93469cff95933c4834fa5c729c6
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2025-02-06 19:27:21 +0600

    listwheel using builder.

--- lib/timeline.dart
+++ lib/timeline.dart
@@ -8,31 +8,27 @@ class TimeLineScreen extends StatefulWidget {
 }
 
 class _TimeLineScreenState extends State<TimeLineScreen> {
+  List<int> items = List.generate(
+    10,
+    (index) => index + 1,
+  );
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     return SizedBox(
-      height: height,
-      child: ListWheelScrollView(itemExtent: 200, children: [
-        Container(
-          color: Colors.pink,
-        ),
-        Container(
-          color: Colors.green,
-        ),
-        Container(
-          color: Colors.blue,
-        ),
-        Container(
-          color: Colors.pink,
-        ),
-        Container(
-          color: Colors.green,
-        ),
-        Container(
-          color: Colors.blue,
-        ),
-      ]),
-    );
+        height: height,
+        child: ListWheelScrollView.useDelegate(
+            itemExtent: 200,
+            childDelegate: ListWheelChildBuilderDelegate(
+              childCount: items.length,
+              builder: (context, index) {
+                return Container(
+                  height: 200,
+                  width: double.infinity,
+                  color: Colors.pink,
+                  child: Center(child: Text('${index}')),
+                );
+              },
+            )));
   }
 }
